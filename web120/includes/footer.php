7a40e32aa98cf10c4ca32c417636bc06bf2d6cea
Ikusey <52266762+Ikusey@users.noreply.github.com>
2019-06-30 22:57:21 -0700

    End of Week 1 Upload

---
+++ web120/includes/footer.php
@@ -0,0 +1,24 @@
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2018 - 2019 by <a href="#" target="_blank">Azriel</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank"><img src="../images/HTML5.png" alt="Valid HTML5!"></a> ~ <a href="http://jigsaw.w3.org/css-validator/check/referer">
+  <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+    </a></small></p>
+</footer>
+<!-- END Footer --> 
+</main>
+<!-- END WRAPPER -->
+
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+    <script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+     </script>
+</body>
+</html>
---
+++ web120/index.php
@@ -0,0 +1,22 @@
+<!--See Bill's training video to learn how to add and use a confic.php file-->
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="picture of Azriel" />
+ <img src="images/phone.png" class="phone" alt="picture of a cat" />
+ <p>My name is Azriel Policarpio. I was born in Seattle, Washington and grew up there. I transferred to Central from Bellevue college, changing my degree to an applied sciene in programming from computer science. I feel its easier for me to start out small and work my way up to the harder things. I hope to learn as much as I can to help me in the future make some thing great.</p>
+    
+<p>Computers have always been apart of my life thanks to my dad's early introduction of them to me from a young age. Video games <strong>dominate</strong> most of my free time, I have a small aspiration to delve into the realm of video game development. Aside from video games I have an interest in <em>Dungeons &amp; Dragons</em>, movies and cartoons/comics.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="picture of otters" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
